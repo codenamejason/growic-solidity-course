af33d7d9db07ef48526fc5c87a21f2f15339b4f7
Jaxcoder <codenamejason@outlook.com>
2022-10-13 21:03:15 -0400

    modifier task

--- packages/hardhat/contracts/UserManagement.sol
+++ packages/hardhat/contracts/UserManagement.sol
@@ -6,11 +6,18 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "hardhat/console.sol";
 
 error InsufficientBalance(uint256 balance);
-error ZeroValue();
+error AmountToSmall();
+error BalanceToSmall();
 error ZeroAddress();
 
-contract UserManagement {
-    enum Status { Pending, Accepted, Rejected }
+contract UserManagement is Ownable {
+    uint256 private Fee = 0.001 ether;
+
+    enum Status {
+        Pending,
+        Accepted,
+        Rejected
+    }
     struct Student {
         address studentId;
         string name;
@@ -18,17 +25,28 @@ contract UserManagement {
         Status status;
         uint256 balance;
     }
-    
+
     mapping(address => Student) public students;
-    
-    function deposit(uint256 amount) public payable {
-        if(amount == 0) revert ZeroValue();
-        if(msg.sender == address(0)) revert ZeroAddress();
+
+    modifier hasDeposited(address studentId) {
+        if (students[studentId].balance == 0) revert BalanceToSmall();
+        _;
+    }
+
+    function addFund(uint256 amount) public hasDeposited(msg.sender) {
+        if (amount < Fee) revert AmountToSmall();
         students[msg.sender].balance += amount;
     }
-    
-    function withdraw(uint256 amount) public {
-        if(students[msg.sender].balance < amount) revert InsufficientBalance(students[msg.sender].balance);
+
+    function deposit(uint256 amount) public payable {
+        if (amount == 0) revert AmountToSmall();
+        if (msg.sender == address(0)) revert ZeroAddress();
+        students[msg.sender].balance += amount;
+    }
+
+    function withdraw(uint256 amount) public onlyOwner {
+        if (students[msg.sender].balance < amount)
+            revert InsufficientBalance(students[msg.sender].balance);
         students[msg.sender].balance -= amount;
         payable(msg.sender).transfer(amount);
     }
@@ -41,11 +59,27 @@ contract UserManagement {
         return students[userAddress].balance;
     }
 
-    function setUserDetails(string memory name, uint256 age) public {
-        students[msg.sender] = Student(msg.sender, name, age, Status.Pending, 0);
+    function setNewUserDetails(string memory name, uint256 age) public {
+        students[msg.sender] = Student(
+            msg.sender,
+            name,
+            age,
+            Status.Pending,
+            0
+        );
     }
-    
-    function getUserDetails(address userAddress) public view returns (Student memory) {
+
+    function updateUserDetails(string memory name, uint256 age) public {
+        Student storage student = students[msg.sender];
+        student.name = name;
+        student.age = age;
+    }
+
+    function getUserDetails(address userAddress)
+        public
+        view
+        returns (Student memory)
+    {
         return students[userAddress];
     }
 }
